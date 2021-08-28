affd48f17d53f585616253039bfc54fffc88aa90
Giang Cá Khô <83223418+TranKienGiang@users.noreply.github.com>
2021-08-28 23:24:44 +0700

    update web.php

--- tran_kien_giang/laravel_routing/web.php
+++ tran_kien_giang/laravel_routing/web.php
@@ -22,23 +22,29 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
 Route::get('/name', function () {
     return ('Well Come To Giang Cá Khô From Ba Vì !!');
 });
+
 Route::get('/input-get', function () {
    return view('inputget');
 });
+
 Route::get('/user{id}', function ($id) {
     return 'User '.$id;
 });
-Route::get('/demoget', function (Request $request) {
-    return view('outputget',['data'=>$request->all()]);
+
+Route::get('/demo-get', function (Request $request) {
+    return view('outputget', ['data'=>$request->all()]);
 });
+
 Route::get('/input-post', function () {
    return view('inputpost');
 });
-Route::post('/demopost', function (Request $request) {
-    return view('outputpost',['data'=>$request->all()]);
+
+Route::post('/demo-post', function (Request $request) {
+    return view('outputpost', ['data'=>$request->all()]);
 });
 require __DIR__.'/auth.php';
     
