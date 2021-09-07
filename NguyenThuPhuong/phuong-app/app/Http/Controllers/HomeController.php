c29dfb378af49d4490a5bbc78bc0a6a9af2fa63b
nguyenthuphuong <Phuongnyo98@gmail.com>
2021-09-07 14:36:37 +0700

    [NTPhuong] - Show data theme

---
+++ NguyenThuPhuong/phuong-app/app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Products;
+use App\Models\Product;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $products = DB::table('products')->paginate(8);
+
+        return view('home-page', ['products' => $products]);
+    }
+    public function shop()
+    {
+        $products = DB::table('products')->paginate(8);
+
+        return view('my-directory.shop-ms', ['products' => $products]);
+    }
+    public function wishlist()
+    {
+        $products = DB::table('products')->get();
+
+        return view('my-directory.wishlist-ms', ['products' => $products]);
+    }
+    public function productSingle($id)
+    {
+        $product = DB::table('products')->find($id);
+        $products = DB::table('products')->paginate(4);
+
+
+        return view('my-directory.product-single-ms', ['product' => $product, 'products' => $products]);
+    }
+}
